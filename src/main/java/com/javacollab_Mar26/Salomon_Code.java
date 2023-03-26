3a2b510c140c3aa49b23d85d8a9f109d5a8e03ac
salfue1002 <salomonfuentes1002@gmail.com>
2023-03-26 13:20:20 -0400

    updates on saloon class

--- src/main/java/com/javacollab_Mar26/Salomon_Code.java
+++ src/main/java/com/javacollab_Mar26/Salomon_Code.java
@@ -1,9 +1,42 @@
 package com.javacollab_Mar26;
 
-public class Salomon_Code {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Salomon_Code {
 	
-	//Testing pushing code 
-	
+	public static void main(String[] args) throws InterruptedException {
+		ChromeOptions options = new ChromeOptions();
+		options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+		options.addArguments("--incognito");
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("http://tutorialsninja.com/demo");
+		driver.findElement(By.linkText("My Account")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.linkText("Login")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.id("input-email")).sendKeys("seleniumpanda@gmail.com");
+		Thread.sleep(2000);
+
+		driver.findElement(By.id("input-password")).sendKeys("Selenium@123");
+		Thread.sleep(2000);
+
+		driver.findElement(By.cssSelector("input.btn.btn-primary")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.linkText("Logout")).click();
+		
+		
+	}
 	
 }
