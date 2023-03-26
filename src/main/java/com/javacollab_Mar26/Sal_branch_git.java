e55e8fb57cd0fbfdd23e204da3de16dee61eb68e
TsegayeT <segayet8@gmail.com>
2023-03-26 14:08:36 -0400

    Added Sal Branch and new class

---
+++ src/main/java/com/javacollab_Mar26/Sal_branch_git.java
@@ -0,0 +1,9 @@
+package com.javacollab_Mar26;
+
+public class Sal_branch_git {
+
+	public static void main(String[] args) {
+		System.out.println("adding new branched to github for practice");
+	}
+
+}
