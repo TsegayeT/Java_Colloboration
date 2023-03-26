ed6b9d2eccdde52a58c6a466ecca9cd54993e09d
TsegayeT <segayet8@gmail.com>
2023-03-26 14:02:54 -0400

    added j loop to ThomasBranch

--- src/main/java/com/javacollab_Mar26/New_BranchClass.java
+++ src/main/java/com/javacollab_Mar26/New_BranchClass.java
@@ -3,7 +3,11 @@ package com.javacollab_Mar26;
 public class New_BranchClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int i=1; i<20;i++) {
+			for(int j=i; j<=7;j++) {
+				System.out.print("*");
+			}System.out.println(" ");
+		}
 
 	}
 
