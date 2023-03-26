fff0886227c14ed2f83d46894dda70b761222425
TsegayeT <segayet8@gmail.com>
2023-03-26 13:12:55 -0400

    added for loop

--- src/main/java/com/javacollab_Mar26/Collab_Mar26.java
+++ src/main/java/com/javacollab_Mar26/Collab_Mar26.java
@@ -3,7 +3,11 @@ package com.javacollab_Mar26;
 public class Collab_Mar26 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int i=1; i<=7; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}System.out.println(" ");
+		}
 
 	}
 
