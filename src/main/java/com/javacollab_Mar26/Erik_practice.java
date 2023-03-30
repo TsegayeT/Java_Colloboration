a37c1196254c897de30100b327ff5f3565978328
Erocwtc <erikgonzalez891@gmail.com>
2023-03-30 19:22:31 -0400

    practice

---
+++ src/main/java/com/javacollab_Mar26/Erik_practice.java
@@ -0,0 +1,12 @@
+package com.javacollab_Mar26;
+
+public class Erik_practice {
+//you guys sucks
+	
+	public static void main(String[] args) {
+		for(int i=1; i<20;i++) {
+			for(int j=i; j<=7;j++) {
+				System.out.print("*");
+			}System.out.println(" ");
+}
+}}
