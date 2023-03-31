f66978ffa8a3a70d7e1b0d4fc888cdb8e3482bf0
Erocwtc <erikgonzalez891@gmail.com>
2023-03-30 20:12:37 -0400

    Updated code for practice

--- src/main/java/com/javacollab_Mar26/Erik_practice.java
+++ src/main/java/com/javacollab_Mar26/Erik_practice.java
@@ -9,4 +9,8 @@ public class Erik_practice {
 				System.out.print("*");
 			}System.out.println(" ");
 }
-}}
+		for(int i=1; i<=7; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}System.out.println(" ");
+}}}
